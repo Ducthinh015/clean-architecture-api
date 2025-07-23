37d69808da8f38b85936458d27335fed92e92af2
Ducthinh015 <thinh0408205@gmail.com>
2025-07-23 14:02:22 +0700

    🚀 Init Clean Architecture API - Based on Alex's repo

---
+++ CleanArchitecture.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using CleanArchitecture.Domain.DomainEvents;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Interfaces.Repositories;
+using CleanArchitecture.Domain.Notifications;
+using CleanArchitecture.Infrastructure.Database;
+using CleanArchitecture.Infrastructure.EventSourcing;
+using CleanArchitecture.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        string migrationsAssemblyName,
+        string connectionString)
+    {
+        // Add event store db context
+        services.AddDbContext<EventStoreDbContext>(
+            options =>
+            {
+                options.UseSqlServer(
+                    connectionString,
+                    b => b.MigrationsAssembly(migrationsAssemblyName));
+            });
+
+        services.AddDbContext<DomainNotificationStoreDbContext>(
+            options =>
+            {
+                options.UseSqlServer(
+                    connectionString,
+                    b => b.MigrationsAssembly(migrationsAssemblyName));
+            });
+
+        // Core Infra
+        services.AddScoped<IUnitOfWork, UnitOfWork<ApplicationDbContext>>();
+        services.AddScoped<IEventStoreContext, EventStoreContext>();
+        services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+        services.AddScoped<IDomainEventStore, DomainEventStore>();
+        services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+        // Repositories
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ITenantRepository, TenantRepository>();
+
+        return services;
+    }
+}
